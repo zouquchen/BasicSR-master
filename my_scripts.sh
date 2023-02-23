461316dac480269077154b1397bfd6e766729cdb
xiaoyanlu <10971659+xiaoyanlu@user.noreply.gitee.com>
2023-02-23 18:40:44 +0800

    不训练了，直接测试

--- my_scripts.sh
+++ my_scripts.sh
@@ -1,10 +1,18 @@
 # 训练ESRGAN
-python basicsr/train.py -opt options/train/ESRGAN/train_ESRGAN_x4_zqc.yml
-
+# python basicsr/train.py -opt options/train/ESRGAN/train_ESRGAN_x4_zqc.yml
 
 # 训练LDL
-python basicsr/train.py -opt options/train/LDL/train_LDL_Real_x4_zqc.yml
-
+# python basicsr/train.py -opt options/train/LDL/train_LDL_Real_x4_zqc.yml
 
 # 训练realesrgan
-python basicsr/train.py -opt options/train/RealESRGAN/train_realesrgan_x4plus_zqc.yml
+# python basicsr/train.py -opt options/train/RealESRGAN/train_realesrgan_x4plus_zqc.yml
+
+
+
+# 测试ESRGAN
+python basicsr/test.py -opt options/test/ESRGAN/test_ESRGAN_x4_zqc.yml
+python basicsr/test.py -opt options/test/EDSR/test_EDSR_Mx4_zqc.yml
+python basicsr/test.py -opt options/test/EDSR/test_EDSR_Lx4_zqc.yml
+python basicsr/test.py -opt options/test/SRResNet_SRGAN/test_MSRGAN_x4_zqc.yml
+python basicsr/test.py -opt options/test/SRResNet_SRGAN/test_MSRResNet_x4_zqc.yml
+
